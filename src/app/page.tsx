6b004bee3e3e8c5e8fa63634ee2121a1143eee54
Yellow987 <daryldsouza123@gmail.com>
2023-05-02 20:34:25 -0700

    adds image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <main>
       hi
+      <Image src="/YellowVRC_1.png" alt="My Image" width={500} height={500} />
     </main>
   )
 }
