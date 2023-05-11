ef118ac72d4aaf4022b146b8530a099af0d5d41f
Yellow987 <daryldsouza123@gmail.com>
2023-05-10 19:46:41 -0700

    adds image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Test from '../markdown/test.mdx';
 import { Prose } from '@nikolovlazar/chakra-ui-prose'
 import React from 'react';
+import { Box } from '@chakra-ui/react';
 
 export default function Home() {
   React.createElement(Test)
@@ -10,14 +11,20 @@ export default function Home() {
 
   return (
     <>
-      
       hello world
-      <Image src="/YellowVRC_1.png" alt="My Image" width={500} height={500} />
+      <Box style={{ width: "100%", height: "500px", minHeight: "500px", position: "relative" }} >
+        <Image
+          src="/YellowVRC_2.png"
+          alt="My Image"
+          fill
+          style={{ objectFit: "cover", objectPosition: "center 20%" }}
+          quality={100}
+        />  
+      </Box>
       end
       <Prose>
         <Test />
       </Prose>
-      
     </>
-  )
+  );
 }
